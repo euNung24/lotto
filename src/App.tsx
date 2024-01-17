9abf1548d28abe855b41a7b685563d8392e4ba5d
eunung <pandamon24@naver.com>
2024-01-17 23:09:10 +0900

    feat: 보너스 번호 구분

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import LottoGame from "./components/LottoGame";
 import styled from "@emotion/styled";
 import LottoBall from "./components/LottoBall";
@@ -39,6 +39,33 @@ const Button = styled.button`
 const WinningBallWrapper = styled.div`
   display: flex;
   gap: 12px;
+`;
+
+const Plus = styled.div`
+  width: 36px;
+  height: 36px;
+  position: relative;
+
+  &::before {
+    content: "";
+    width: 6px;
+    height: 60%;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #000;
+  }
+  &::after {
+    content: "";
+    width: 6px;
+    height: 60%;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%) rotate(90deg);
+    background-color: #000;
+  }
 `;
 
 const genWinningNumbers = () => {
@@ -160,9 +187,17 @@ function App() {
         <section>
           <h3>로또 당첨 번호</h3>
           <WinningBallWrapper>
-            {winningNumbers.map((num) => (
-              <LottoBall key={num} num={num} />
-            ))}
+            {winningNumbers.map((num, idx) => {
+              if (idx === winningNumbers.length - 1) {
+                return (
+                  <Fragment key={num}>
+                    <Plus />
+                    <LottoBall num={num} />
+                  </Fragment>
+                );
+              }
+              return <LottoBall key={num} num={num} />;
+            })}
           </WinningBallWrapper>
         </section>
       )}
